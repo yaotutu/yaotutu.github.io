8fcf1d60850a080ff6160a71cd62e3fb662f6f6d
yaotutu <957810668@qq.com>
2024-09-08 14:34:18 +0800

    🔧 更新(astro.config.mjs)：修改站点URL和基础路径设置   这能确保在新的GitHub Pages上正确部署和解析。

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.yaotutu.top",
+  site: "https://yaotutu.github.io",
+  base: "",
   style: {
     scss: {
       includePaths: ["./src/styles"],
