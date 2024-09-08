489df95fbb9190e2f95d56489ce43a517315d74e
yaotutu <957810668@qq.com>
2024-09-08 12:28:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Yaotutu Blog
+
+## TODO
+* 首页图片优化，适配夜间模式
---
+++ src/consts.ts
@@ -0,0 +1,88 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Site title and description
+export const SITE_TAB = "Yaotutu";
+export const SITE_TITLE = "Yaotutu 🧊";
+export const SITE_DESCRIPTION = "Yaotutu blog";
+export const DATE_FORMAT = "YYYY-MM-DD";
+
+// User profile information
+export const USER_NAME = "Yaotutu";
+export const USER_AVATAR = "/profile.png";
+
+// Server and transition settings
+export const SERVER_URL = "";
+
+// Some informative text on the site
+export const infoTest = {
+  tag: "Tag: ",
+  noTag: "untagged",
+  tagCard: "Tags",
+  tagPage: "Tag - ",
+  noCategory: "uncategorized",
+  categoryCard: "Categories",
+  categoryPage: "Category - ",
+  link: "Link: ",
+  prevPage: "Recent posts",
+  nextPage: "Older posts",
+};
+
+// Menu items for navigation
+export const menuItems = [
+  { id: "home", text: "Home", href: "/", svg: "home", target: "_self" }, // Home page
+  { id: "about", text: "About", href: "/about", svg: "about", target: "_self" }, // About page
+  {
+    id: "blog",
+    text: "Blogs",
+    href: "/blog",
+    svg: "blog",
+    target: "_self",
+    subItems: [
+      {
+        id: "all",
+        text: "All blogs",
+        href: "/blog",
+        svg: "post",
+        target: "_self",
+      }, // All blog
+      {
+        id: "tech",
+        text: "Tech blogs",
+        href: "/blog/categories/tech",
+        svg: "cube",
+        target: "_self",
+      }, // Technology category
+    ],
+  }, // Blog page with sub-items
+  {
+    id: "project",
+    text: "Project",
+    href: "/project",
+    svg: "project",
+    target: "_self",
+  }, // Projects page
+  {
+    id: "contact",
+    text: "Contact",
+    href: "/contact", // Contact email
+    target: "_self", // Open in a new tab
+    svg: "contact",
+  },
+];
+
+// Social media and contact icons
+export const socialIcons = [
+  {
+    href: "https://github.com/yaotutu",
+    ariaLabel: "Github",
+    title: "Github",
+    svg: "github",
+  },
+  {
+    href: "/rss.xml",
+    ariaLabel: "RSS Feed",
+    title: "RSS Feed",
+    svg: "rss",
+  },
+];
