59810661f49f3dfa2d62164a2b7f72c072dd144b
yaotutu <957810668@qq.com>
2024-11-18 23:18:09 +0800

    feat: Add marked library for markdown rendering
    This commit adds the "marked" library as a dependency in the package.json file. It also imports the "marked" module in the rss.xml.js file and uses it to render the content of each blog post in the RSS feed.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,7 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { marked } from "marked";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -8,9 +9,13 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: posts.map((post) => {
+      const { body } = post;
+      return {
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+        content: marked(body),
+      };
+    }),
   });
 }
